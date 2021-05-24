b5c0c1894c432050a56f6165eb07108cdf86f29b
shahbaazkyz <shahbaazkyz@gmail.com>
2021-05-24 13:18:01 +0500

    V1

---
+++ app.js
@@ -0,0 +1,45 @@
+var mins = 00;
+var secs = 00;
+var mSecs = 00;
+
+
+var dMinutes = document.getElementById('mins');
+var dSeconds = document.getElementById('secs');
+var dMiliSeconds = document.getElementById('mSecs');
+
+var interval;
+
+function start() {
+   interval =  setInterval(function () {
+        mSecs++;
+        dMiliSeconds.innerHTML = mSecs;
+        if (mSecs >= 100) {
+            secs++;
+            dSeconds.innerHTML = secs;
+            mSecs = 00;
+        }
+        
+        else if (secs >= 59) {
+            mins++;
+            dMinutes.innerHTML = mins;
+            secs = 00;
+        }
+    }, 10);
+    document.getElementById('start').disabled = true;
+}
+
+function pause() {
+        document.getElementById('start').disabled = false;
+
+    clearInterval(interval)
+}
+
+function stop() {
+    mins = 00;
+    secs = 00;
+    mSecs = 00;
+    dMinutes.innerHTML = mins
+    dSeconds.innerHTML = secs
+    dMiliSeconds.innerHTML = mSecs
+
+}
